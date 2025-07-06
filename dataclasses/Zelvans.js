ba35dcae16fff6c36b7be166abb3982cd44d3a0b
Lassudrie <161130219+Lassudrie@users.noreply.github.com>
2025-07-06 18:40:45 +0200

    Use placeholder image for new civs

--- dataclasses/Zelvans.js
+++ dataclasses/Zelvans.js
@@ -2,7 +2,7 @@ import Civilisation from './Civilisation.js';
 
 const zelvans = new Civilisation(
   'Zelvans',
-  'images/avatars/zelvan.png',
+  'assets/main menu.png',
   ['Amphibious Physiology', 'Bioluminescent Communication', 'Stealth in Aquatic Environments'],
   'The Zelvans evolved in the deep oceans of the planet Zelvaris. Covered in iridescent scales that shimmer with emotional hues, they built majestic underwater cities powered by geothermal vents. Their society values ritual, tradition, and the spiritual guidance of the Oracles. Contact with surface-dwelling species has led to conflict, especially with the rapidly expanding humans, whose industrial pollution once poisoned a sacred sea trench.',
   'Spiritual Theocracy',
