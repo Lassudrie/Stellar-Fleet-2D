a34cc35b15999c38888afa2e6d4489667ae9ae33
Lassudrie <161130219+Lassudrie@users.noreply.github.com>
2025-07-06 17:57:12 +0200

    Add Zelvans civilisation dataclass and update example

--- civilisationExample.js
+++ civilisationExample.js
@@ -1,4 +1,6 @@
 import humans from './dataclasses/Humans.js';
+import zelvans from './dataclasses/Zelvans.js';
 import { describeCivilisation } from './logic/civilisationLogic.js';
 
 console.log(describeCivilisation(humans));
+console.log(describeCivilisation(zelvans));
---
+++ dataclasses/Zelvans.js
@@ -0,0 +1,12 @@
+import Civilisation from './Civilisation.js';
+
+const zelvans = new Civilisation(
+  'Zelvans',
+  'images/avatars/zelvan.png',
+  ['Amphibious Physiology', 'Bioluminescent Communication', 'Stealth in Aquatic Environments'],
+  'The Zelvans evolved in the deep oceans of the planet Zelvaris. Covered in iridescent scales that shimmer with emotional hues, they built majestic underwater cities powered by geothermal vents. Their society values ritual, tradition, and the spiritual guidance of the Oracles. Contact with surface-dwelling species has led to conflict, especially with the rapidly expanding humans, whose industrial pollution once poisoned a sacred sea trench.',
+  'Spiritual Theocracy',
+  'Humans'
+);
+
+export default zelvans;
